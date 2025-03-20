edbf20a0e98427b8009c229420b50df78501d337
Anton_Sakaev <pfenixi@gmail.com>
2025-03-20 23:40:11 +1200

    Vsyo

--- app/src/main/java/com/example/winterschool/screens/searchscreen/SearchStateScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.winterschool.screens.searchscreen
-
-import com.example.domain.models.Books
-
-data class SearchStateScreen(
-    val postList: List<Books> = emptyList(),
-    val isLoading: Boolean = true,
-    val errorMessage: String? = null,
-)
