c630d9f315aea16d263aa02b195b36a28c34f450
anton <sakaev.dev@yandex.ru>
2025-10-13 17:20:41 +1200

    Оптимизирован импорт

--- presentation/src/main/java/com/example/presentation/screens/components/items/BookCard.kt
+++ presentation/src/main/java/com/example/presentation/screens/components/items/BookCard.kt
@@ -1,35 +1,20 @@
 package com.example.presentation.screens.components.items
 
-import android.content.Context
-import android.widget.Toast
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -40,11 +25,8 @@ import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.example.domain.remote.models.Items
 import com.example.presentation.R
-import com.example.presentation.screens.components.icons.Favorite
 import com.example.presentation.screens.components.showToast
 import com.example.presentation.screens.searchscreen.SearchScreenViewModel
-import com.example.presentation.theme.LightGray
-import com.example.presentation.theme.Red
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
